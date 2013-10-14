28b88b97e3f641fb3ff5147b888b01c9bc020716
Sam Khan <samk1200@gmail.com>
2013-10-14 09:55:08 -0400

    added global gitignore

--- install.bash
+++ install.bash
@@ -7,23 +7,16 @@ for file in `find . -name '*.sym' | cut -c 3- | rev | cut -c 5- | rev`
 do
   original="$HOME/.$file"
 
-  # only install dotfiles for programs that exist on the system
-  if [ -e "$original" ] || [ -h "$original" ]; then
-    # make backup if it doesn't exist yet
-    if [ ! -e "$backup_dir.$file" ]; then
-      echo "Backing up $original $backup_dir"
-      mv "$original" "$backup_dir"
-    fi
+  # make backup if it doesn't exist yet
+  if [ ! -e "$backup_dir.$file" ]; then
+    echo "Backing up $original $backup_dir"
+    mv "$original" "$backup_dir"
+  fi
 
-    # symlink the new file into home
-    if [ ! -e "$original" ]; then
-      echo "ln -s `pwd`/$file.sym $original"
-      ln -s "`pwd`/$file.sym" "$original"
-    fi
-
-  # original doesn't exist
-  else
-    echo "skipping $original"
+  # symlink the new file into home
+  if [ ! -e "$original" ]; then
+    echo "ln -s `pwd`/$file.sym $original"
+    ln -s "`pwd`/$file.sym" "$original"
   fi
 done
 
