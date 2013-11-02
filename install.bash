a7f791b9b4506be88f43e56f1c3aa86d3ba38c40
Sam Khan <samk1200@gmail.com>
2013-11-02 15:02:59 -0400

    downloads vim#pathogen and colorscheme

--- install.bash
+++ install.bash
@@ -2,26 +2,35 @@ backup_dir="$HOME/.dotfiles_old/"
 
 mkdir -p "$backup_dir"
 
-#get files ending in .sym and remove .sym
+# symlink all .sym files into home dir
 for file in `find . -name '*.sym' | cut -c 3- | rev | cut -c 5- | rev`
 do
   original="$HOME/.$file"
 
-  # make backup if it doesn't exist yet
+  # make backup if a backup doesn't exist yet
   if [ ! -e "$backup_dir.$file" ]; then
     echo "Backing up $original $backup_dir"
     mv "$original" "$backup_dir"
   fi
 
-  # symlink the new file into home
+  # symlink the file into home
   if [ ! -e "$original" ]; then
     echo "ln -s `pwd`/$file.sym $original"
     ln -s "`pwd`/$file.sym" "$original"
   fi
 done
 
-if [ -e "$HOME/.zshrc" ]; then
-  source "$HOME/.zshrc"
-else
-  source "$HOME/.bashrc"
+#install vim pathogen
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+  mkdir -p ~/.vim/autoload ~/.vim/bundle
+  curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 fi
+
+#install vim colorscheme
+if [ ! -e ~/.vim/bundle/molokai ]; then
+  git clone https://github.com/tomasr/molokai ~/.vim/bundle/molokai
+fi
+
+#source new bash file
+source ~/.bashrc
