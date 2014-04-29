e452d68a5ef521b62ea129fb90d3215ed5c25c70
Sam Khan <samk1200@gmail.com>
2014-04-29 04:36:08 +0000

    updated url of pathogen

--- install.bash
+++ install.bash
@@ -33,7 +33,7 @@ done
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
   mkdir -p ~/.vim/autoload ~/.vim/bundle
   curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+    https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 fi
 
 #install vim colorscheme
